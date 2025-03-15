1b5e50e1ba9d12c6e359375d480ebc85d810d823
MaXiMo000 <109451965+MaXiMo000@users.noreply.github.com>
2025-03-15 16:30:15 +0530

    used authwrapper

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Sidebar from "./components/Sidebar";  // Sidebar navigation
+import Sidebar from "./components/Sidebar";
+import AuthWrapper from "./components/AuthWrapper"; // ✅ Import AuthWrapper
+
 import AdminDashboard from "./pages/AdminDashboard";
 import AdminQuizzes from "./pages/AdminQuizzes";
 import AdminReports from "./pages/AdminReports";
@@ -20,46 +22,49 @@ import AdminWrittenTestReports from "./pages/AdminWrittenTestReports";
 import UserWrittenReports from "./pages/UserWrittenReports";
 import UserWrittenReportCheck from "./pages/UserWrittenReportsCheck";
 import Leaderboard from "./pages/Leaderboard";
-import "./App.css"; // Import global styles
+import "./App.css";
 
 const App = () => {
     return (
         <Router>
-            <Sidebar /> {/* Sidebar always visible */}
-            <div className="main-content"> {/* Ensures proper spacing */}
-                <Routes>
-                    {/* 🔹 Public Routes */}
-                    <Route path="/" element={<Home />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
+            <Routes>
+                {/* 🔹 Public Routes */}
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
 
-                    {/* 🔹 Admin Routes */}
-                    <Route path="/admin" element={<AdminDashboard />} />
-                    <Route path="/admin/create" element={<AdminQuizzes />} />
-                    <Route path="/admin/report" element={<AdminReports />} />
-
-                    <Route path="/admin/quiz/:id" element={<QuizQuestions />} />
-                    <Route path="/admin/written-tests" element={<AdminWrittenTests />} />
-                    <Route path="/admin/written-test/question/:id" element={<TestQuestions />} />
-
-                    <Route path="/admin/written-test/report" element={<AdminWrittenTestReports />} />
-                    
-                    {/* 🔹 User Routes */}
-                    <Route path="/user/test" element={<UserQuizzes />} />
-                    <Route path="/user/test/:id" element={<TakeQuiz />} />
-                    <Route path="/user/report" element={<UserReports />} />
-
-                    <Route path="/report/:id" element={<UserReportsCheck />} />
-
-                    <Route path="/written-tests" element={<UserWrittenTests />} />
-                    <Route path="/take-written-test/:id" element={<TakeWrittenTest />} />
-
-                    <Route path="/user/written-reports" element={<UserWrittenReports />} />
-                    <Route path="/user/written-test-report/:id" element={<UserWrittenReportCheck />} />
-
-                    <Route path="/leaderboard" element={<Leaderboard />} />
-                </Routes>
-            </div>
+                {/* ✅ Private Routes: Wrapped with `AuthWrapper` */}
+                <Route 
+                    path="/*"
+                    element={
+                        <AuthWrapper>
+                            <Sidebar /> {/* Sidebar appears only for logged-in users */}
+                            <div className="main-content">
+                                <Routes>
+                                    <Route path="/" element={<Home />} />
+                                    <Route path="/admin" element={<AdminDashboard />} />
+                                    <Route path="/admin/create" element={<AdminQuizzes />} />
+                                    <Route path="/admin/report" element={<AdminReports />} />
+                                    <Route path="/admin/quiz/:id" element={<QuizQuestions />} />
+                                    <Route path="/admin/written-tests" element={<AdminWrittenTests />} />
+                                    <Route path="/admin/written-test/question/:id" element={<TestQuestions />} />
+                                    <Route path="/admin/written-test/report" element={<AdminWrittenTestReports />} />
+                                    
+                                    {/* User Routes */}
+                                    <Route path="/user/test" element={<UserQuizzes />} />
+                                    <Route path="/user/test/:id" element={<TakeQuiz />} />
+                                    <Route path="/user/report" element={<UserReports />} />
+                                    <Route path="/report/:id" element={<UserReportsCheck />} />
+                                    <Route path="/written-tests" element={<UserWrittenTests />} />
+                                    <Route path="/take-written-test/:id" element={<TakeWrittenTest />} />
+                                    <Route path="/user/written-reports" element={<UserWrittenReports />} />
+                                    <Route path="/user/written-test-report/:id" element={<UserWrittenReportCheck />} />
+                                    <Route path="/leaderboard" element={<Leaderboard />} />
+                                </Routes>
+                            </div>
+                        </AuthWrapper>
+                    }
+                />
+            </Routes>
         </Router>
     );
 };
